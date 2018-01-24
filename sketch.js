dde93afc2bdbfa8ab21c19c6dcd8283cb4873965
erifurusawa <ef2536@columbia.edu>
2018-01-24 16:54:38 -0500

    changed colors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+var pg;
+
 function setup() {
-  // put setup code here
+	createCanvas(800, 400);
+	pg = createGraphics(400, 250)
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+	// background(255);
+	fill(0, 12);
+	// rect(0, 0, 150, 400, 0);
+	fill(0,145,156);
+	noStroke();
+	ellipse(mouseX, mouseY, 60, 60);
+
+
+	pg.background(255);
+	pg.noFill(255);
+	pg.stroke(255);
+	pg.ellipse(mouseX, mouseY, 40, 40);
+	pg.fill(255);
+	pg.text ('Hi', mouseX, mouseY);
+
+	image(pg, 200, 75);
+	text ('Hi', mouseX, mouseY, 100);
+
+	fill(246,139,105);
+	text(mouseX, 400, 200);
 }
+
